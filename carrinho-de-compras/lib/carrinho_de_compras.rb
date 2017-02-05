3543340dc74d4f53395cff3d232323fcb38f56a5
marlonfurtado <marlonfurtado.rs@gmail.com>
2017-02-05 00:17:06 -0200

    Teste com rspec

---
+++ carrinho-de-compras/lib/carrinho_de_compras.rb
@@ -0,0 +1,13 @@
+class CarrinhoDeCompras
+  attr_reader :itens
+
+  def initialize
+    @itens = []
+  end
+  def <<(item)
+    itens << item
+  end
+  def maior_valor
+    itens.map(&:valor_total).max || 0
+  end
+end
